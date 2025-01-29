6e4a9cb65cf85d848cf5c3e547944328d53d4dc5
JojoVes <JojoVes@users.noreply.github.com>
2025-01-29 15:27:04 -0400

    allow configuration of default metadata display
    When viewing a compound object, prior to these changes it would default
    to displaying the metadata for the currently viewed 'Part', and this
    allows us to switch that to display the 'Compound' metadata since that
    seems to be a desirable default display for some clients.

--- dgi_members.post_update.php
+++ dgi_members.post_update.php
@@ -15,3 +15,14 @@ function dgi_members_post_update_add_params(&$sandbox) {
   ]);
   $config->save(TRUE);
 }
+
+/**
+ * Implements hook_post_update_NAME().
+ */
+function dgi_members_post_update_add_metadata_display_flag(&$sandbox) {
+  $config = \Drupal::configFactory()->getEditable('dgi_members.settings');
+  // If this config has not been set, default to false.
+  $display_compound_object_metadata = $config->get('display_compound_object_metadata_by_default') ?? FALSE;
+  $config->set('display_compound_object_metadata_by_default', $display_compound_object_metadata);
+  $config->save(TRUE);
+}
